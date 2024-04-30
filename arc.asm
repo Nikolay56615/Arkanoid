c54536061f96631d89c89569eff3cc86fcd0afc4
dromankin <d.romankin@g.nsu.ru>
2024-04-30 12:35:55 +0700

    another prototype, not working yet

--- arc.asm
+++ arc.asm
@@ -27,81 +27,87 @@ main>
 #r4 - temporary register for y coordinate to check if ball in brick row
 #r5 - has an adress of brick
 #r6 - value of brick: 1 - not broken,0-broken
-ldi r0,15 #x-coordinate
+ldi r0,16 #x-coordinate
 ldi r1,1 #x-velocity
 ldi r2,1 #y-coordinate
 ldi r3,1 # y-velocity
 move r2,r4
 while
-    cmp r2,0
+    cmp r2,31
 stays ne
-    #negating-velocity section
+    add r1,r0,r4
     if
-        cmp r0,31
+        cmp r4,31
     is ge
-        neg r1 
-    fi
-    if
-        cmp r0,0
-    is le
         neg r1
     fi
     if
-        cmp r2,31
-    is ge
-        neg r2
+        cmp r4,0
+    is le
+        neg r1
     fi
-    #adding coordinates and checking for collision
-    add r1,r0,r0
+    move r4,r0
     add r2,r3,r4
     if
-        cmp r4,17
+        cmp r4,0
+    is le
+        neg r3
+    fi
+    if
+        cmp r4,29
     is eq
-        ldi r5,bricks1
-        add r5,r0,r5
-        ld r5,r6
+        ldi r4,0xbeef
+        push r3 # stack has an y-velocity before collision with bat
+        ldb r4,r3 #now it has coordinate of bat
         if
-            cmp r6,1
+            cmp r0,r3
         is eq
-            dec r6 #here is collision moment - it should be detected by logisim
-            st r5,r6
-            neg r1
+            pop r3
             neg r3
-            add r1,r0,r0
-            add r1,r0,r0
-            add r3,r2,r2
         else
-            move r4,r2
+            dec r3
+            if
+                cmp r0,r3
+            is eq
+                pop r3
+                neg r3
+            else
+                dec r3
+                if
+                    cmp r0,r3
+                is eq
+                    pop r3
+                    neg r3
+                fi
+            fi
         fi
     fi
     if
-    cmp r4,18
-    is eq
-        ldi r5,bricks2
-        add r5,r0,r5
-        ld r5,r6
-        if
-            cmp r6,1
-        is eq
-            dec r6 #here is collision moment - it should be detected by logisim(with trigger and comparator, for example e.g.)
-            st r5,r6
-            neg r1
-            neg r3
-            add r1,r0,r0 #two addings to return x back
-            add r1,r0,r0
-            add r3,r2,r2 #adding negative velocity to y
-        else
-            move r4,r2
-        fi
+        cmp r4, 1
+    is eq, or
+        cmp r4, 2
+    is eq, or
+        
     fi
+    arr
+    add r1,r0,r0
     
-    #move r2,r4
+    
 wend
 halt
+ball_y: dc 28
+x_bat: dc 1
+
 #suppose that field is 32x32, bricks take rows 17 and 18
 asect 0xdead #added this adress to check it in logisim RAM
-bricks1: dc 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-bricks2: dc 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+bricks1_1: dc 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0
+bricks1_2: dc 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0
+
+bricks2_1: dc 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0
+bricks2_2: dc 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0
+
+bricks3_1: dc 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0
+bricks3_2: dc 0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0
 end.
 #ya hz voobsche rabotaet eta zalupa ili net, poka probnyi variant
 # need to change coordinate system, because in this version ball can only reflect by corner of brick
