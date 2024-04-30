7497eae99535a9898ef0317f0e60ea05e2ceb9bd
dromankin <d.romankin@g.nsu.ru>
2024-04-30 22:20:12 +0700

    code prototype not working

--- arc.asm
+++ arc.asm
@@ -24,34 +24,45 @@ default_handler>
 rsect main
 
 main>
-#r4 - temporary register for y coordinate to check if ball in brick row
+#r4 - temporary register for x/y coordinate to pre-calculate
 #r5 - has an adress of brick
-#r6 - value of brick: 1 - not broken,0-broken
+#r6 - score
 ldi r0,16 #x-coordinate
 ldi r1,1 #x-velocity
-ldi r2,1 #y-coordinate
-ldi r3,1 # y-velocity
-move r2,r4
+ldi r2,28 #y-coordinate
+ldi r3,-1 # y-velocity
 while
     cmp r2,31
 stays ne
     add r1,r0,r4
     if
-        cmp r4,31
+        cmp r4,32
     is ge
+        push r1
+        shl r1
         neg r1
+        add r1,r4,r4
+        pop r1
     fi
     if
-        cmp r4,0
+        cmp r4,-1
     is le
+        push r1
+        shl r1
         neg r1
+        add r1,r4,r4
+        pop r1
     fi
     move r4,r0
     add r2,r3,r4
     if
-        cmp r4,0
+        cmp r4,-1
     is le
-        neg r3
+        push r2
+        shl r2
+        neg r2
+        add r2,r4,r4
+        pop r2
     fi
     if
         cmp r4,29
@@ -61,38 +72,17 @@ stays ne
         ldb r4,r3 #now it has coordinate of bat
         if
             cmp r0,r3
-        is eq
+        is le, and
+            sub r3,2
+            cmp r0,r3
+        is ge
+        then
             pop r3
             neg r3
-        else
-            dec r3
-            if
-                cmp r0,r3
-            is eq
-                pop r3
-                neg r3
-            else
-                dec r3
-                if
-                    cmp r0,r3
-                is eq
-                    pop r3
-                    neg r3
-                fi
-            fi
+            add r2,r3,r4
         fi
     fi
-    if
-        cmp r4, 1
-    is eq, or
-        cmp r4, 2
-    is eq, or
-        
-    fi
-    arr
-    add r1,r0,r0
-    
-    
+    move r4,r2    
 wend
 halt
 ball_y: dc 28
